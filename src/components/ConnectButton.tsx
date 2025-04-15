a161497b3645b003700dc97276e5800fd8972e98
Arda Bugra OZER <abozer@gmail.com>
2025-04-15 11:31:08 -0500

    Cleanup. ConnectButton still not fully functional

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -5,11 +5,8 @@ export const ConnectButton = () => {
   return (
     <div >
         <appkit-button /> <br/> <br/>
-        {/* <appkit-wallet-button wallet="metamask" /> */}
-        <br/> <br/>
-        <appkit-connect-button />
-        <br/> <br/>
-        <appkit-network-button />
+        <appkit-wallet-button wallet="metamask" /> <br/> <br/>
+        <appkit-network-button /> <br/> <br/>
     </div>
   )
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,17 @@
 import { cookieStorage, createStorage } from 'wagmi'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum , sepolia, solana, polygonMumbai, solanaTestnet, bitcoin, bitcoinTestnet} from '@reown/appkit/networks'
+import { mainnet, arbitrum , sepolia, solana, polygonMumbai, solanaTestnet, bitcoin, bitcoinTestnet, arbitrumSepolia} from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+//console.log(projectId);
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum, sepolia] as [AppKitNetwork, ...AppKitNetwork[]]
+export const networks = [mainnet, arbitrum, sepolia, arbitrumSepolia ] as [AppKitNetwork, ...AppKitNetwork[]]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
